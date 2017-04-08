eaded314c99ce7bb4d3962a5316c980f798e5aa5
hansenzhao <hansen305@gatech.edu>
2017-04-07 22:31:11 -0400

    Airport using latitude and longitude

--- src/AirportSim.java
+++ src/AirportSim.java
@@ -4,17 +4,18 @@ import java.util.concurrent.ThreadLocalRandom;
 public class AirportSim {
     public static Airport[] airportList = new Airport[2];
     public static final double[][] distAirports = {
-        { 0, 1239.17, 1942.17, 954.95, 2170.93},  //LAX -> LAX, AUS, ATL, SEA, YYZ
-        { 1239.17, 0, 811.47, 1769.19, 1358.23},  //AUS -> ...
-        { 1942.17, 811.47, 0, 2177.79, 740.07},  //ATL -> ...
-        { 954.95, 1769.19, 2177.79, 0, 2054.55},
-        { 2170.93, 1358.23, 740.07, 2054.55, 0}
+            {0, 1239.17, 1942.17, 954.95, 2170.93},  //LAX -> LAX, AUS, ATL, SEA, YYZ
+            {1239.17, 0, 811.47, 1769.19, 1358.23},  //AUS -> ...
+            {1942.17, 811.47, 0, 2177.79, 740.07},  //ATL -> ...
+            {954.95, 1769.19, 2177.79, 0, 2054.55},
+            {2170.93, 1358.23, 740.07, 2054.55, 0}
     };
+
     public static void main(String[] args) {
         //Implemented different airplanes air airports
         int numInitials = 100;
-        airportList[0] = new Airport("LAX", 0.1, 0.1, 0.1,20,10, -118.4, 33.9); //Los Angelas
-        airportList[1] = new Airport("AUS", 0.1, 0.1, 0.1,20,10, -97.6, 30.1); //Austin
+        airportList[0] = new Airport("LAX", 0.1, 0.1, 0.1, 20, 10, -118.4, 33.9); //Los Angelas
+        airportList[1] = new Airport("AUS", 0.1, 0.1, 0.1, 20, 10, -97.6, 30.1); //Austin
 
         double[][] distanceMatrix = new double[airportList.length][airportList.length];
         for (int i = 0; i < airportList.length; i++) {
@@ -25,16 +26,16 @@ public class AirportSim {
                 distanceMatrix[i][j] = distanceMatrix[j][i] = 3959.0 * Math.acos(Math.sin(Math.toRadians(airportList[i].getM_Lat())) *
                         Math.sin(Math.toRadians(airportList[j].getM_Lat())) +
                         Math.cos(Math.toRadians(airportList[i].getM_Lat())) * Math.cos(Math.toRadians(airportList[j].getM_Lat()))
-                                * Math.cos(Math.toRadians(airportList[i].getM_Lat()-airportList[j].getM_Lat())));
+                                * Math.cos(Math.toRadians(airportList[i].getM_Long() - airportList[j].getM_Long())));
             }
         }
         Simulator.stopAt(1000);
         //In each loop, new planes will depart at every airport
-        for (int i=0; i<numInitials; i++ ){
-            Airplane boe747_1 = new Airplane("Boe747", 614,416);
-            Airplane boe747_2 = new Airplane("Boe747", 614,416);
-            Airplane boe747_3 = new Airplane("Boe747", 614,416);
-            Airplane boe747_4 = new Airplane("Boe747", 614,416);
+        for (int i = 0; i < numInitials; i++) {
+            Airplane boe747_1 = new Airplane("Boe747", 614, 416);
+            Airplane boe747_2 = new Airplane("Boe747", 614, 416);
+            Airplane boe747_3 = new Airplane("Boe747", 614, 416);
+            Airplane boe747_4 = new Airplane("Boe747", 614, 416);
             Airplane a380 = new Airplane("A380", 634, 853);
             // The departure event, when handled, will automatically assign a new number of passengers randomly. Thus no need to assign randomly here.
             AirportEvent departureEvent_1 = new AirportEvent(0, airportList[0], AirportEvent.PLANE_DEPARTS, boe747_1, 0, 0);
@@ -51,3 +52,4 @@ public class AirportSim {
         Simulator.run();
     }
 }
+
